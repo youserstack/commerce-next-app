b2775fba5a097d8ec8f274ba5fc59605a88b0645
youserstack <youserstack@gmail.com>
2024-04-05 18:59:55 +0900

    update

--- next.config.js
+++ next.config.js
@@ -3,14 +3,14 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    // NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
+    NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     // MONGODB_URI: process.env.MONGODB_URI,
     // CLOUD_NAME: "dzktdrw7o",
     // CLOUD_API_KEY: process.env.CLOUD_API_KEY,
     // CLOUD_API_SECRET: process.env.CLOUD_API_SECRET,
     // CLOUDINARY_PRESET: "next_commerce_app_upload_preset",
     // CLOUDINARY_API_BASE_URL: "https://api.cloudinary.com/v1_1/dzktdrw7o/upload",
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
   images: {
     domains: [
