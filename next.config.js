f8bde9c566fe393567829c19954f93559501a5c8
youserstack <youserstack@gmail.com>
2024-04-05 23:31:05 +0900

    update

--- next.config.js
+++ next.config.js
@@ -30,33 +30,33 @@ const nextConfig = {
       "k.kakaocdn.net",
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        // source: "/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/:path*",
+  //       // source: "/api/:path*",
+  //       headers: [
+  //         {
+  //           key: "Access-Control-Allow-Origin",
+  //           value: "*",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Methods",
+  //           value: "GET, POST, PUT, DELETE, OPTIONS",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value:
+  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Credentials",
+  //           value: "true",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
   async rewrites() {
     return [
       {
