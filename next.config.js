1c8beba830478d2436faf7392d2bd06d7c676dd2
youserstack <youserstack@gmail.com>
2024-04-05 19:11:14 +0900

    update

--- next.config.js
+++ next.config.js
@@ -4,8 +4,13 @@ const nextConfig = {
   reactStrictMode: false,
   env: {
     NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
+
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NAVER_CLIENT_ID: process.env.NAVER_CLIENT_ID,
+    NAVER_CLIENT_SECRET: process.env.NAVER_CLIENT_SECRET,
+
     MONGODB_URI: process.env.MONGODB_URI,
+
     CLOUD_NAME: process.env.CLOUD_NAME,
     CLOUD_API_KEY: process.env.CLOUD_API_KEY,
     CLOUD_API_SECRET: process.env.CLOUD_API_SECRET,
@@ -24,33 +29,33 @@ const nextConfig = {
       "k.kakaocdn.net",
     ],
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       // source: "/:path*",
-  //       source: "/api/:path*",
-  //       headers: [
-  //         {
-  //           key: "Access-Control-Allow-Origin",
-  //           value: "*",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Methods",
-  //           value: "GET, POST, PUT, DELETE, OPTIONS",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Headers",
-  //           value:
-  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Credentials",
-  //           value: "true",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        // source: "/:path*",
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
