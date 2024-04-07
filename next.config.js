21b382fed0e2ca53f3771690fb3c018c80f9b6b6
youserstack <youserstack@gmail.com>
2024-04-07 13:19:05 +0900

    update

--- next.config.js
+++ next.config.js
@@ -6,11 +6,7 @@ const nextConfig = {
     NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
     NEXT_PRODUCTION_ENV: process.env.NEXT_PRODUCTION_ENV,
 
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    // NEXTAUTH_URL:
-    //   process.env.NODE_ENV === "production"
-    //     ? process.env.NEXT_PRODUCTION_ENV + "/api/auth"
-    //     : process.env.NEXT_PUBLIC_ENV + "/api/auth",
+    // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NAVER_CLIENT_ID: process.env.NAVER_CLIENT_ID,
     NAVER_CLIENT_SECRET: process.env.NAVER_CLIENT_SECRET,
@@ -35,33 +31,33 @@ const nextConfig = {
       "k.kakaocdn.net",
     ],
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/:path*",
-  //       // source: "/api/:path*",
-  //       headers: [
-  //         {
-  //           key: "Access-Control-Allow-Origin",
-  //           value: "*",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Methods",
-  //           value: "GET, POST, PUT, DELETE, OPTIONS",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Headers",
-  //           value:
-  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Credentials",
-  //           value: "true",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        // source: "/:path*",
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+        ],
+      },
+    ];
+  },
   // async rewrites() {
   //   return [
   //     {
