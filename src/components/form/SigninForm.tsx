fdd2015e48fe72c99140f8f5210516dd25e7e841
youserstack <youserstack@gmail.com>
2024-04-17 18:47:27 +0900

    update

--- src/components/form/SigninForm.tsx
+++ src/components/form/SigninForm.tsx
@@ -38,6 +38,7 @@ export default function SigninForm() {
     await signIn("credentials", {
       email,
       password,
+      redirect: true,
       callbackUrl: "/my/orders",
     });
     // console.log({response})
--- src/pages/api/v3/auth/signout/index.ts
+++ src/pages/api/v3/auth/signout/index.ts
@@ -2,6 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log("\n[api/v3/auth/signout]");
-  res.setHeader("Set-Cookie", [`refreshToken=;Max-Age=0;Path=/`]);
+  res.setHeader("Set-Cookie", "refreshToken=;Max-Age=0;Path=/");
   res.status(200).json({ message: "Logged Out." });
 }
