d3764584fdba863302f4ce3625fdf7dc7f06dac2
youserstack <youserstack@gmail.com>
2024-04-17 18:36:24 +0900

    update

--- src/pages/api/v3/auth/signout/index.ts
+++ src/pages/api/v3/auth/signout/index.ts
@@ -1,5 +1,7 @@
-export default async function handler(req: any, res: any) {
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log("\n[api/v3/auth/signout]");
-  res.setHeader("Set-Cookie", [`refreshToken=;Max-Age=-1;Path=/;SameSite=Strict;HttpOnly`]);
+  res.setHeader("Set-Cookie", [`refreshToken=;Max-Age=0;Path=/`]);
   res.status(200).json({ message: "Logged Out." });
 }
